fa648962317ff3178339694b3c368c1c9fb54bf3
Dilan Emir Angeles Martinez <230110252@itsoeh.edu.mx>
2024-12-10 22:44:23 -0600

    CURSO-DSA
    Signed-off-by: Dilan Emir Angeles Martinez <230110252@itsoeh.edu.mx>

---
+++ learning/Stacks/.ipynb_checkpoints/stack_sandbox-checkpoint.py
@@ -0,0 +1,9 @@
+from Node import Node
+import logging
+
+class Stack():
+    def __init__(self, top_item = None):
+        self.top_item = top_item
+
+    def peek(self):
+        return self.top_item.get_value()
